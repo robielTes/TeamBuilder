c00188274abb66282bdb85d9542dbcd840c36a8c
robielcpnv <robiel.tesfazghi@cpnv.ch>
2021-11-12 10:02:38 +0100

    version1: Début Examen - Robiel

--- view/Homepage.php
+++ view/Homepage.php
@@ -13,6 +13,7 @@
     <h1 class="text-6xl font-bold text-center">TeamBuilder</h1>
     <p class="text-right -mb-3 text-xl">Vous êtes connecté en tant que : <strong><?=$_SESSION['user']->name?></strong></p>
 </header>
+<p class="text-xs">Robiel Tesfazghi CPNV</p>
 <div class="flex pt-4">
     <a class="bg-green-800 hover:bg-green-600 text-white font-bold py-3 px-4 rounded mx-4" href="?action=listMember">MemberList</a>
     <a class="bg-green-800 hover:bg-green-600 text-white font-bold py-3 px-4 rounded mx-4" href="?action=listTeam">Mes équipes</a>
